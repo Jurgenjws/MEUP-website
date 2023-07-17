39332b17bca8c3aaf05836331f0a5b5c5c0537a5
Jurgenjws <113023523+Jurgenjws@users.noreply.github.com>
2023-07-17 21:37:41 +0200

    Add files via upload

--- js/rad.js
+++ js/rad.js
@@ -1,13 +1,41 @@
-const imgBtn = document.querySelector(".rad")
+const meupBtn = document.querySelector(".meupBtn")
+const drankBtn = document.querySelector(".drankBtn")
+
+const meupRadContainer = document.querySelector(".meup-rad-container")
+const meupRadBtn = document.querySelector(".meup-rad")
+
+const drankRadContainer = document.querySelector(".drank-rad-container")
+const drankRadBtn = document.querySelector(".drank-rad")
+
 const vakken = 24;
 const anglePerVak = 360 / vakken;
-const minAngleVerschil = 30;
 
-imgBtn.addEventListener('click', function () {
+meupBtn.addEventListener('click', function () {
+    meupRadContainer.style.display = 'block'
+    drankRadContainer.style.display = 'none'
+});
+
+drankBtn.addEventListener('click', function () {
+    meupRadContainer.style.display = 'none'
+    drankRadContainer.style.display = 'block'
+});
+
+
+
+meupRadBtn.addEventListener('click', function () {
     const angle = Math.floor(Math.random() * 360) * 10 + 1000;
-    const validAngle = Math.round(angle / anglePerVak) * anglePerVak;
+    // const validAngle = Math.round(angle / anglePerVak) * anglePerVak;
 
-    imgBtn.style.transform = `rotate(${validAngle}deg)`;
-    imgBtn.style.transition = `transform 1s ease-out`;
+    meupRadBtn.style.transform = `rotate(${angle}deg)`;
+    meupRadBtn.style.transition = `transform 1s ease-out`;
+
+});
+
+drankRadBtn.addEventListener('click', function () {
+    const angle = Math.floor(Math.random() * 360) * 10 + 1000;
+    // const validAngle = Math.round(angle / anglePerVak) * anglePerVak;
+
+    drankRadBtn.style.transform = `rotate(${angle}deg)`;
+    drankRadBtn.style.transition = `transform 1s ease-out`;
 
 });
