7790045a71776b762cb1094a7bd7f6fb227a269f
Jurgenjws <113023523+Jurgenjws@users.noreply.github.com>
2023-07-25 22:54:00 +0200

    Add files via upload

--- js/rad.js
+++ js/rad.js
@@ -1,41 +1,41 @@
-const meupBtn = document.querySelector(".meupBtn")
-const drankBtn = document.querySelector(".drankBtn")
+const meupBtnDutch = document.querySelector(".meupBtnDutch")
+const meupBtnEnglish = document.querySelector(".meupBtnEnglish")
 
-const meupRadContainer = document.querySelector(".meup-rad-container")
-const meupRadBtn = document.querySelector(".meup-rad")
+const meupRadContainerDutch = document.querySelector(".meup-rad-container-dutch")
+const meupRadDutchBtn = document.querySelector(".meup-rad-dutch")
 
-const drankRadContainer = document.querySelector(".drank-rad-container")
-const drankRadBtn = document.querySelector(".drank-rad")
+const meupRadContainerEnglish = document.querySelector(".meup-rad-container-english")
+const meupRadEnglishBtn = document.querySelector(".meup-rad-english")
 
 const vakken = 24;
 const anglePerVak = 360 / vakken;
 
-meupBtn.addEventListener('click', function () {
-    meupRadContainer.style.display = 'block'
-    drankRadContainer.style.display = 'none'
+meupBtnDutch.addEventListener('click', function () {
+    meupRadContainerDutch.style.display = 'block'
+    meupRadContainerEnglish.style.display = 'none'
 });
 
-drankBtn.addEventListener('click', function () {
-    meupRadContainer.style.display = 'none'
-    drankRadContainer.style.display = 'block'
+meupBtnEnglish.addEventListener('click', function () {
+    meupRadContainerDutch.style.display = 'none'
+    meupRadContainerEnglish.style.display = 'block'
 });
 
 
 
-meupRadBtn.addEventListener('click', function () {
+meupRadDutchBtn.addEventListener('click', function () {
     const angle = Math.floor(Math.random() * 360) * 10 + 1000;
     // const validAngle = Math.round(angle / anglePerVak) * anglePerVak;
 
-    meupRadBtn.style.transform = `rotate(${angle}deg)`;
-    meupRadBtn.style.transition = `transform 1s ease-out`;
+    meupRadDutchBtn.style.transform = `rotate(${angle}deg)`;
+    meupRadDutchBtn.style.transition = `transform 1s ease-out`;
 
 });
 
-drankRadBtn.addEventListener('click', function () {
+meupRadEnglishBtn.addEventListener('click', function () {
     const angle = Math.floor(Math.random() * 360) * 10 + 1000;
     // const validAngle = Math.round(angle / anglePerVak) * anglePerVak;
 
-    drankRadBtn.style.transform = `rotate(${angle}deg)`;
-    drankRadBtn.style.transition = `transform 1s ease-out`;
+    meupRadEnglishBtn.style.transform = `rotate(${angle}deg)`;
+    meupRadEnglishBtn.style.transition = `transform 1s ease-out`;
 
 });
